7298df1dda50a4fada3314715415bcd79c30fc31
kaceykniffen <kaceykniffen@gmail.com>
2023-06-04 21:57:36 -0500

    Init nre Solid JS project

---
+++ project/src/App.tsx
@@ -0,0 +1,15 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      <section>
+      </section>
+    </div>
+  );
+};
+
+export default App;
